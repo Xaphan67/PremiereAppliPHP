a4300d25163c7fda663f31d52f39db4d7ef1d28b
Xaphan67 <cedric.falda@gmail.com>
2023-07-13 16:33:02 +0200

    Ajout style avec bootstrap

--- index.php
+++ index.php
@@ -4,35 +4,52 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
     <title>Ajout produit</title>
 </head>
 
 <body>
-    <h1>Ajouter un produit</h1>
-    <form action="traitement.php" method="post">
-        <p>
-            <label>
-                Nom du produit :
-                <input type="text" name="name">
-            </label>
-        </p>
-        <p>
-            <label>
-                Prix du produit :
-                <input type="number" step="any" name="price">
-            </label>
-        </p>
-        <p>
-            <label>
-                Quantité désirée :
-                <input type="number" name="qtt" value="1">
-            </label>
-        </p>
-        <p>
-            <input type="submit" name="submit" value="Ajouter le produit">
-        </p>
-    </form>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-sm-2">
+
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Ajouter un produit</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="recap.php">Récapitulatif</a>
+                    </li>
+                </ul>
+            </nav>
+            <article class="col-sm-6">
+                <h1>Ajouter un produit</h1>
+                <form action="traitement.php" method="post">
+                    <div class="form-group row">
+                        <label class="col-sm-3 col-form-label col-form-label" for="name">Nom du produit :</label>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control" id="name" name="name">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-3 col-form-label col-form-label" for="price">Prix du produit :</label>
+                        <div class="col-sm-4">
+                            <input type="number" class="form-control" step="any" id="price" name="price">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-3 col-form-label col-form-label" for="qtt">Quantité désirée :</label>
+                        <div class="col-sm-4">
+                            <input type="number" class="form-control" name="qtt" id="qtt" value="1">
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="submit">Ajouter le produit</button>
+                </form>
+            </article>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- recap.php
+++ recap.php
@@ -7,47 +7,67 @@ session_start();
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
     <title>Récapitulatif des produits</title>
 </head>
 
 <body>
-    <?php
-    if (!isset($_SESSION['products']) || empty($_SESSION['products'])) { // Si la clé products n'existe pas en session ou qu'elle existe mais est vide...
-        echo "<p>Aucun produit en session...</p>>";
-    } else { // Sinon, la clé existe et n'est pas vide. On l'affiche dans un tableau
-        echo "
-            <table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nom</th>
-                        <th>Prix</th>
-                        <th>Quantité</th>
-                        <th>Total</th>
-                    </tr>
-                </thead>
-                <tbody>";
-        $totalGeneral = 0;
-        foreach ($_SESSION['products'] as $index => $product) {
-            echo "
-                <tr>
-                    <td>" . $index . "</td>
-                    <td>" . $product["name"] . "</td>
-                    <td>" . number_format($product["price"], 2, ",", "&nbsp;") . "&nbsp;€</td>
-                    <td>" . $product["qtt"] . "</td>
-                    <td>" . number_format($product["total"], 2, ",", "&nbsp;") . "&nbsp;€</td>
-                </tr>";
-            $totalGeneral += $product["total"];
-        }
-        echo "
-                <tr>
-                    <td colspan=4>Total général : </td>
-                    <td><strong>" . number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€</strong></td>
-                </tbody>
-            </table>";
-    }
-    ?>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-sm-2">
+
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Ajouter un produit</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Récapitulatif</a>
+                    </li>
+                </ul>
+            </nav>
+            <article class="col-sm-6">
+                <h1>Récapitulatif</h1>
+                <?php
+                if (!isset($_SESSION['products']) || empty($_SESSION['products'])) { // Si la clé products n'existe pas en session ou qu'elle existe mais est vide...
+                    echo "<p>Aucun produit en session...</p>>";
+                } else { // Sinon, la clé existe et n'est pas vide. On l'affiche dans un tableau
+                    echo '
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nom</th>
+                                    <th>Prix</th>
+                                    <th>Quantité</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>';
+                    $totalGeneral = 0;
+                    foreach ($_SESSION['products'] as $index => $product) {
+                        echo "
+                            <tr>
+                                <td>" . $index . "</td>
+                                <td>" . $product["name"] . "</td>
+                                <td>" . number_format($product["price"], 2, ",", "&nbsp;") . "&nbsp;€</td>
+                                <td>" . $product["qtt"] . "</td>
+                                <td>" . number_format($product["total"], 2, ",", "&nbsp;") . "&nbsp;€</td>
+                            </tr>";
+                        $totalGeneral += $product["total"];
+                    }
+                    echo "
+                                <tr>
+                                    <td colspan=4>Total général : </td>
+                                    <td><strong>" . number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€</strong></td>
+                                </tbody>
+                            </table>";
+                }
+                ?>
+            </article>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
